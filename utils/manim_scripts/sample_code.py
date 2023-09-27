cbe70d784eb11a84b7655005e217e7294d00a9b8
Aqib Ahmed <aqibahmed@Aqibs-MacBook-Pro.local>
2023-09-27 10:28:47 -0700

    Minor changes

--- utils/manim_scripts/sample_code.py
+++ utils/manim_scripts/sample_code.py
@@ -5,34 +5,11 @@ class GeneratedCode(ThreeDScene):
         axes = ThreeDAxes()
         self.add(axes)
 
-        # Parameters for the Gaussian distribution
-        mu = 0
-        sigma = 1
+        # Plotting the parabola point by point
+        parabola = ParametricFunction(lambda t: [t, t**2, 0], t_range=[-2, 2], color=YELLOW)
+        self.play(Create(parabola))
 
-        # Generate the Gaussian curve
-        x = np.linspace(-5, 5, 100)
-        y = (1 / (sigma * np.sqrt(2 * np.pi))) * np.exp(-0.5 * ((x - mu) / sigma) ** 2)
+        # Adding coordinate labels
+        axes.add_coordinates()
 
-        # Plot the Gaussian curve
-        curve = axes.plot(x, y, color=BLUE)
-
-        # Number of dots to generate
-        num_dots = 100
-
-        # Generate random x-coordinates for the dots
-        x_coords = np.random.normal(mu, sigma, num_dots)
-
-        # Generate the falling dots
-        dots = VGroup()
-        for x_coord in x_coords:
-            dot = Dot(radius=0.05, color=RED)
-            dot.move_to(axes.c2p(x_coord, 5, 0))  # Start the dots at the top of the scene
-            dots.add(dot)
-
-        # Animate the falling dots
-        self.play(Create(curve))
-        self.play(Create(dots))
-        self.play(dots.animate.arrange(DOWN, buff=0.1).shift(UP * 5), run_time=3)
-
-        self.wait()from manim import *
-import numpy as np
+        self.wait()
